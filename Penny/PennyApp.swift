0947e1c2d3133db739f1639e86ea34fdb9ddaab4
Vaibbies <vaibhavjasti@gmail.com>
2026-02-03 14:00:49 -0800

    Initial Commit

---
+++ Penny/PennyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyApp.swift
+//  Penny
+//
+//  Created by Vaibhav Jasti on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PennyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PennyTests/PennyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PennyTests.swift
+//  PennyTests
+//
+//  Created by Vaibhav Jasti on 2/3/26.
+//
+
+import Testing
+@testable import Penny
+
+struct PennyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
