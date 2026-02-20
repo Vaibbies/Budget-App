b7f5146ee4334748ac3fd19a6359e15d448f4b24
Vaibbies <vaibhavjasti@gmail.com>
2026-02-19 21:19:29 -0800

    Update ContentView.swift

--- Penny/Core/ContentView.swift
+++ Penny/Core/ContentView.swift
@@ -2,16 +2,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-<<<<<<< Updated upstream:Penny/Core/ContentView.swift
         MainAppView()
-=======
         ZStack {
             Color.red.ignoresSafeArea()
             Text("HELLO PENNY")
                 .font(.largeTitle)
                 .foregroundColor(.white)
         }
->>>>>>> Stashed changes:Penny/ContentView.swift
     }
 }
 
